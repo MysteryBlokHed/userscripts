851d60425326953983ff5e5835ca0d7e4bffbceb
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-01-02 23:53:53 -0500

    Add OmeTV Grabber script

---
+++ OmeTVGrabber/OmeTVGrabber.user.ts
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name        OmeTV Grabber
+// @description Get IP addresses of users on OmeTV
+// @version     0.1.0
+// @author      Adam Thompson-Sharpe
+// @match       *://*.ome.tv/*
+// @grant       none
+// ==/UserScript==
+;(() => {
+  /** OmeTV hijacks most logging functions, but they forgot about groups for some reason */
+  const groupLog = (...data: any[]) => {
+    console.groupCollapsed(...data)
+    console.groupEnd()
+  }
+
+  let lastCandidateType: RTCIceCandidateType | null
+  let currentIp: string | null = 'Not Found'
+
+  let ipEl: HTMLSpanElement | undefined
+
+  const setupIpContainer = () => {
+    /* Set up container for IP */
+    const buttonArea = document.querySelector('.buttons')
+    if (!buttonArea) setTimeout(setupIpContainer, 1000)
+    const ipContainer = document.createElement('div')
+    ipContainer.style.textAlign = 'center'
+    ipEl = document.createElement('span')
+    ipEl.style.fontWeight = 'bold'
+    ipEl.innerText = `IP: ${currentIp}`
+    ipContainer.appendChild(ipEl)
+    buttonArea?.prepend(ipContainer)
+  }
+  setupIpContainer()
+
+  /**
+   * Proxy handler for the RTCPeerConnection.prototype.addIceCandidate function
+   */
+  const addIceCandidateHandler: ProxyHandler<
+    typeof RTCPeerConnection['prototype']['addIceCandidate']
+  > = {
+    apply(target, thisArg: RTCPeerConnection, args: [RTCIceCandidate, ...any]) {
+      const candidate = args[0]
+
+      console.groupCollapsed('Candidate', candidate, 'added')
+      groupLog('Type:\t', candidate.type)
+      groupLog('Address:\t', candidate.address)
+      groupLog('Related:\t', candidate.relatedAddress)
+      console.groupEnd()
+
+      if (candidate.type === 'relay' && lastCandidateType !== 'relay') {
+        currentIp = candidate.address
+        groupLog('!!! IP FOUND !!! ', candidate.address)
+        if (ipEl) ipEl.innerText = `IP: ${candidate.address}`
+      }
+
+      lastCandidateType = candidate.type
+      return Reflect.apply(target, thisArg, args)
+    },
+  }
+
+  /** Update the IP display when connections are closed */
+  const closeHandler: ProxyHandler<
+    typeof RTCPeerConnection['prototype']['close']
+  > = {
+    apply(target, thisArg: RTCPeerConnection, args) {
+      if (ipEl && currentIp !== 'Not Found') {
+        ipEl.innerText = `IP: Not Found (Last: ${currentIp})`
+        currentIp = 'Not Found'
+      }
+      return Reflect.apply(target, thisArg, args)
+    },
+  }
+
+  RTCPeerConnection.prototype.addIceCandidate = new Proxy(
+    RTCPeerConnection.prototype.addIceCandidate,
+    addIceCandidateHandler
+  )
+
+  RTCPeerConnection.prototype.close = new Proxy(
+    RTCPeerConnection.prototype.close,
+    closeHandler
+  )
+})()
---
+++ OmeTVGrabber/README.md
@@ -0,0 +1,8 @@
+# OmeTV Grabber
+
+Get IP addresses of users on OmeTV.
+
+## Use
+
+Once the script is installed, the current user's IP will be displayed
+under the video box. When not connected, the IP of the last user will be displayed.
