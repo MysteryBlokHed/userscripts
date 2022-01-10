3ee44e4ddd5a810e597433355d1ecc1500f78384
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-01-10 16:54:02 -0500

    Add Poll Everywhere script
    Adds a script to deanonymize answers on Poll Everywhere (pollev.com).
    For some reason, even when answers are set to be anonymous, the name of
    the submitter is still sent; the element containing it is just hidden.

---
+++ PollEvReveal/PollEvReveal.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name        Poll Everywhere (pollev.com) Deanonymizer
+// @description Show the names of people who submitted answers on Poll Everywhere
+// @version     0.1.0
+// @author      Adam Thompson-Sharpe
+// @match       *://*.pollev.com/*
+// ==/UserScript==
+;(() => {
+  /**
+   * Show all participant elements on an element
+   * @param {HTMLElement} el
+   */
+  const showParticipants = el =>
+    el
+      .querySelectorAll('.component-response-qa__result__participant')
+      .forEach(el => (el.hidden = false))
+
+  /** Observe the answers element for changes */
+  const observer = new MutationObserver(mutationsList => {
+    for (const mutation of mutationsList) {
+      console.log(mutation)
+      if (mutation.type === 'childList') {
+        // If the participant element was removed, add it back
+        const removed = Array.from(mutation.removedNodes)
+        if (
+          removed.length &&
+          removed.some(el => el.className === 'component-response-qa__result')
+        ) {
+          mutation.target.appendChild(removed[0])
+        }
+
+        // Unhide participants
+        mutation.target
+          .querySelectorAll('.component-response-qa__result__participant')
+          .forEach(el => (el.hidden = false))
+      }
+    }
+  })
+
+  // Wait until the answers element exists before trying to observe it
+  const answersPoll = setInterval(() => {
+    /** Element containing everybody's answers */
+    const answers = document.querySelector('.component-list')
+    if (answers) {
+      showParticipants(document.body)
+      observer.observe(answers, { childList: true, subtree: true })
+      clearInterval(answersPoll)
+    }
+  }, 1000)
+})()
---
+++ PollEvReveal/README.md
@@ -0,0 +1,9 @@
+# PollEvReveal
+
+Show the names of people who submitted answers on Poll Everywhere
+
+## Use
+
+Once the script is installed, you will be able to see the names of the people
+who submitted answers to questions.
+The name will appear right under the answer text as the answers come in.
