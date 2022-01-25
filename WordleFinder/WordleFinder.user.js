492dc8ec9460e3f79d37d6797ce3069763db10a9
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2022-01-24 19:43:24 -0500

    Add a bunch of things
    Added things:
    - 'adieu' as default word instead of a random one
    - Better regex generation for possible words
    - Check if the word was already found
    - Maybe some other thins I forgot

--- WordleFinder/WordleFinder.user.js
+++ WordleFinder/WordleFinder.user.js
@@ -62,12 +62,12 @@
     for (const char of guess) press(char)
     press('Enter')
   }
-  /** Get a list of finished rows */
+  /** Get a list of finished rows' shadowRoots */
   const finishedRows = () => {
     const rows = Array.from(gameRoot.querySelectorAll('game-row[letters]'))
-    return rows.filter(row =>
-      row.shadowRoot?.querySelector('game-tile[evaluation]'),
-    )
+    return rows
+      .filter(row => row.shadowRoot?.querySelector('game-tile[evaluation]'))
+      .map(el => el.shadowRoot)
   }
   /** Get the last submitted row */
   const lastRow = () => {
@@ -80,10 +80,10 @@
     lastRow()?.querySelectorAll('game-tile[evaluation=correct]').length === 5
   /** Update the unused, misplaced, and correct letters based on the last word */
   const updateLetters = () => {
-    console.log('updating letters')
-    console.log('last:', lastRow())
+    console.log('Updating letters')
+    console.log('Last row:', lastRow())
     lastRow()
-      ?.shadowRoot?.querySelectorAll('game-tile[evaluation]')
+      ?.querySelectorAll('game-tile[evaluation]')
       .forEach((el, i) => {
         console.log('el,i:', el, i)
         // Get the result of each tile
@@ -98,9 +98,9 @@
         else if (evaluation === 'correct')
           correctLetters.push([el.getAttribute('letter') ?? '', i])
       })
-    console.log('unused:', unusedLetters)
-    console.log('present:', misplacedLetters)
-    console.log('correct:', correctLetters)
+    console.log('Unused letters:', unusedLetters)
+    console.log('Misplaced letters:', misplacedLetters)
+    console.log('Correct letters:', correctLetters)
   }
   /** Get a regular expression to match dictionary words with */
   const dictRegex = () => {
@@ -113,21 +113,40 @@
         continue
       }
       let letters = 'abcdefghijklmnopqrstuvwxyz'
-      // Remove unused letters from bank
+      // Remove unused letters
       unusedLetters.forEach(letter => (letters = letters.replace(letter, '')))
+      // If there are any letters not in this position, don't use them
+      const filteredMisplaced = misplacedLetters.filter(
+        ([_, place]) => place === i,
+      )
+      filteredMisplaced.forEach(
+        ([letter]) => (letters = letters.replace(letter, '')),
+      )
       regexp += `[${letters}]`
     }
-    console.log('generated expression:', regexp)
-    return new RegExp(regexp, 'i')
+    console.log('Expression for word:', regexp)
+    return new RegExp(`^${regexp}$`, 'i')
   }
+  // If the Wordle is already done, don't do anything
+  if (wasCorrect()) return console.log('Word already found')
   let attempts = 1
-  const randomWord = wordList[Math.floor(Math.random() * wordList.length)]
-  submitGuess(randomWord)
-  // Start with a random word
-  if (wasCorrect()) return console.log('Word found:', randomWord)
+  // Use 'adieu' as the first word since there are 4 vowels
+  submitGuess('adieu')
+  if (wasCorrect()) return console.log('Word found: adieu')
+  /** Loop to guess words */
   const guess = () => {
-    if (attempts > 5) throw new Error('Could not find word')
-    const nextWord = wordList.find(word => word.match(dictRegex()))
+    if (attempts > 6) throw new Error('Could not find word')
+    const regex = dictRegex()
+    /**
+     * The next word to guess.
+     * Found by finding a word in the dictionary that matches the generated regex
+     * and that contains all existing but misplaced letters
+     */
+    const nextWord = wordList.find(
+      word =>
+        word.match(regex) &&
+        misplacedLetters.every(([letter]) => word.includes(letter)),
+    )
     if (!nextWord) throw new Error('No matching words left in dictionary')
     submitGuess(nextWord)
     if (wasCorrect()) return console.log('Word found:', nextWord)
--- WordleFinder/WordleFinder.user.ts
+++ WordleFinder/WordleFinder.user.ts
@@ -77,12 +77,12 @@
     press('Enter')
   }
 
-  /** Get a list of finished rows */
+  /** Get a list of finished rows' shadowRoots */
   const finishedRows = () => {
     const rows = Array.from(gameRoot.querySelectorAll('game-row[letters]'))
-    return rows.filter(row =>
-      row.shadowRoot?.querySelector('game-tile[evaluation]'),
-    ) as HTMLElement[]
+    return rows
+      .filter(row => row.shadowRoot?.querySelector('game-tile[evaluation]'))
+      .map(el => el.shadowRoot)
   }
 
   /** Get the last submitted row */
@@ -98,10 +98,10 @@
 
   /** Update the unused, misplaced, and correct letters based on the last word */
   const updateLetters = () => {
-    console.log('updating letters')
-    console.log('last:', lastRow())
+    console.log('Updating letters')
+    console.log('Last row:', lastRow())
     lastRow()
-      ?.shadowRoot?.querySelectorAll('game-tile[evaluation]')
+      ?.querySelectorAll('game-tile[evaluation]')
       .forEach((el, i) => {
         console.log('el,i:', el, i)
 
@@ -118,9 +118,9 @@
           correctLetters.push([el.getAttribute('letter') ?? '', i])
       })
 
-    console.log('unused:', unusedLetters)
-    console.log('present:', misplacedLetters)
-    console.log('correct:', correctLetters)
+    console.log('Unused letters:', unusedLetters)
+    console.log('Misplaced letters:', misplacedLetters)
+    console.log('Correct letters:', correctLetters)
   }
 
   /** Get a regular expression to match dictionary words with */
@@ -136,29 +136,49 @@
       }
 
       let letters = 'abcdefghijklmnopqrstuvwxyz'
-      // Remove unused letters from bank
+      // Remove unused letters
       unusedLetters.forEach(letter => (letters = letters.replace(letter, '')))
+      // If there are any letters not in this position, don't use them
+      const filteredMisplaced = misplacedLetters.filter(
+        ([_, place]) => place === i,
+      )
+      filteredMisplaced.forEach(
+        ([letter]) => (letters = letters.replace(letter, '')),
+      )
 
       regexp += `[${letters}]`
     }
 
-    console.log('generated expression:', regexp)
+    console.log('Expression for word:', regexp)
 
-    return new RegExp(regexp, 'i')
+    return new RegExp(`^${regexp}$`, 'i')
   }
+
+  // If the Wordle is already done, don't do anything
+  if (wasCorrect()) return console.log('Word already found')
 
   let attempts = 1
 
-  const randomWord = wordList[Math.floor(Math.random() * wordList.length)]
-  submitGuess(randomWord)
+  // Use 'adieu' as the first word since there are 4 vowels
+  submitGuess('adieu')
+  if (wasCorrect()) return console.log('Word found: adieu')
 
-  // Start with a random word
-  if (wasCorrect()) return console.log('Word found:', randomWord)
-
+  /** Loop to guess words */
   const guess = () => {
-    if (attempts > 5) throw new Error('Could not find word')
+    if (attempts > 6) throw new Error('Could not find word')
 
-    const nextWord = wordList.find(word => word.match(dictRegex()))
+    const regex = dictRegex()
+
+    /**
+     * The next word to guess.
+     * Found by finding a word in the dictionary that matches the generated regex
+     * and that contains all existing but misplaced letters
+     */
+    const nextWord = wordList.find(
+      word =>
+        word.match(regex) &&
+        misplacedLetters.every(([letter]) => word.includes(letter)),
+    )
     if (!nextWord) throw new Error('No matching words left in dictionary')
     submitGuess(nextWord)
 
